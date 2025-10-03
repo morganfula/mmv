b3d7a74fdbdd17f31c4f5cf7d16e2d753d036a6a
morganfula <morgan.fula@me.com>
2025-10-03 08:24:35 +0200

    fix menu nav bug

--- plugins/close-nav.client.js
+++ plugins/close-nav.client.js
@@ -1,10 +1,25 @@
+// export default defineNuxtPlugin(() => {
+// 	const router = useRouter();
+
+// 	router.afterEach(() => {
+// 		if (general.isNavOpen) {
+// 			general.isNavOpen = false;
+// 			toggleMenu(false);
+// 		}
+// 	});
+// });
+
+// plugins/close-nav.client.js
+import { general } from '@/store';
+import { toggleMenu } from '~/composables/toggleMenu'; // <<< important
+
 export default defineNuxtPlugin(() => {
 	const router = useRouter();
 
 	router.afterEach(() => {
 		if (general.isNavOpen) {
 			general.isNavOpen = false;
-			toggleMenu(false);
+			toggleMenu(false); // referme l’anim du burger proprement
 		}
 	});
 });
--- plugins/gsap.client.js
+++ plugins/gsap.client.js
@@ -1,5 +1,19 @@
-export default defineNuxtPlugin(async () => {
+// export default defineNuxtPlugin(async () => {
+// 	if (process.client) {
+// 		gsap.registerPlugin(ScrollTrigger);
+// 	}
+// });
+
+// plugins/gsap.client.js
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+export default defineNuxtPlugin(() => {
 	if (process.client) {
 		gsap.registerPlugin(ScrollTrigger);
+		// Optionnel: exposer gsap si tu veux l’injecter ailleurs via useNuxtApp()
+		return {
+			provide: { gsap, ScrollTrigger },
+		};
 	}
 });
